29ba5501e7556bec2165040dbc520a5807d01c0f
“cecvak” <c.vakaloudi@gmail.com>
2020-05-14 14:27:39 +0200

    working on news

---
+++ src/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.header-slider').slick({
+      dots: true,
+      arrows: false,
+      // autoplay: true,
+      // responsive: [
+      //   {
+      //     breakpoint: 1024,
+      //     settings: {
+      //       infinite: true,
+      //       dots: true,
+      //       arrows: true,
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 600,
+      //     settings: {
+      //       arrows: true,
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 480,
+      //     settings: {
+      //       arrows: true,
+
+      //     }
+      //   }
+      //   // You can unslick at a given breakpoint now by adding:
+      //   // settings: "unslick"
+      //   // instead of a settings object
+      // ]
+    });
+  });
+      
